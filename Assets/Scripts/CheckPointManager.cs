b8d233ed4fdac2ad1bbbcbea80e4d61734d0c9f8
Ivan Tortosa Berenguer <ivantb08@hotmail.com>
2023-02-19 12:57:54 +0100

    cosas

--- Assets/Scripts/CheckPointManager.cs
+++ Assets/Scripts/CheckPointManager.cs
@@ -6,13 +6,21 @@ public class CheckPointManager : MonoBehaviour
 {
     [SerializeField]  private GameObject player1;
     [SerializeField]  private GameObject player2;
-    
+    [SerializeField] private GameObject enemy;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
+        if(PlayerPrefs.GetFloat("Y") == 0f)
+        {
+            PlayerPrefs.SetFloat("Y", 90f);
+        }
         player1.transform.rotation = Quaternion.Euler(0, PlayerPrefs.GetFloat("Y"), 0);
-        player2.transform.rotation = Quaternion.Euler(0, PlayerPrefs.GetFloat("Y") -5f, 0);
+        player2.transform.rotation = Quaternion.Euler(0, PlayerPrefs.GetFloat("Y") - 5f, 0);
+        enemy.transform.rotation = Quaternion.Euler(0, PlayerPrefs.GetFloat("Y") + 12f, 0);
+
     }
 
 }
